7b849bf11433b480a928982ebc9b6513037f00bc
Asya Nikitina <anikitina@cnm.edu>
2018-08-03 09:07:15 -0600

    Made modificatons in class methods

--- php/snap-magic-method.php
+++ php/snap-magic-method.php
@@ -1,32 +1,14 @@
 <?php
 
 Class Person {
-	private $personName;
 	private $personAge;
+	private $personName;
 
 	public function __construct(string $newPersonName, int $newPersonAge) {
 		$this->setPersonName($newPersonName);
 		$this->SetPersonAge($newPersonAge);
 	}
 
-	public function getPersonName(): string {
-		return ($this->personName);
-
-	}
-
-	public function setPersonName(string $newPersonName) {
-		$newPersonName = trim($newPersonName);
-
-		if(empty($newPersonName) | !ctype_alpha($newPersonName)) {
-			throw(new \InvalidArgumentException("Name is empty or contains characters other than letters."));
-		}
-		// verify that a message is shorter than 64 characters
-		if(strlen($newPersonName) > 64) {
-			throw(new \RangeException("Name is too long."));
-		}
-		// store the valid message in the class state variable
-		$this->personName = $newPersonName;
-	}
 
 	public function getPersonAge(): int {
 		return ($this->personAge);
@@ -36,7 +18,6 @@ Class Person {
 		if($newPersonAge < 0) {
 			throw(new \RangeException("Person's age cannot be less than 0."));
 		}
-
 		$this->personAge = $newPersonAge;
 	}
 
@@ -51,16 +32,29 @@ Class Person {
 		}
 	}
 
+	public function getPersonName(): string {
+		return ($this->personName);
+	}
+
+	public function setPersonName(string $newPersonName) {
+		$newPersonName = trim($newPersonName);
+		if(empty($newPersonName) | !ctype_alpha($newPersonName)) {
+			throw(new \InvalidArgumentException("Name is empty or contains characters other than letters."));
+		}
+		// verify that a message is shorter than 64 characters
+		if(strlen($newPersonName) > 64) {
+			throw(new \RangeException("Name is too long."));
+		}
+		// store the valid message in the class state variable
+		$this->personName = $newPersonName;
+	}
 
 	public function __toString() : string {
-		$psersonInfo = "<table><tr><th>Name: " .$this->getPersonName()
-			."</th><th>Age: " .$this->getPersonAge() ."</th></tr></table>";
+		$psersonInfo = "\n" ."Name: " .$this->getPersonName() ."\nAge: " .$this->getPersonName();
 		return $psersonInfo;
 	}
 }
 
-// test class Person
-
 $newPerson = new Person("James", 25);
-$newPerson->greetPerson();
-$newPerson->__toString();
+echo $newPerson->greetPerson();
+echo $newPerson->__toString();
